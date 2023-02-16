d248014405fb0451c9ca95fcff98c4179de3ad76
only1smokey <bozhidarbotev683@gmail.com>
2023-02-16 16:07:48 +0100

    c#DEBUG

---
+++ TBDSchool/c#DEBUG/DisplayCs/DisplayCs/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DisplayCs
+{
+    public partial class Form1 : Form
+    {
+
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        public void Attach(TextBox textBox)
+        {
+            
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            string buttonText = button.Text;
+
+            if (buttonText == "Enter")
+            {
+                inputTxtBox.AppendText(Environment.NewLine);
+            }
+            else if (buttonText == "Space")
+            {
+                inputTxtBox.AppendText(" ");
+            }
+            else if (buttonText == "Backspace")
+            {
+                if (inputTxtBox.Text.Length > 0)
+                {
+                    inputTxtBox.Text = inputTxtBox.Text.Substring(0, inputTxtBox.Text.Length - 1);
+                }
+            }
+            else
+            {
+                inputTxtBox.AppendText(buttonText);
+            }
+        }
+    }
+
+}
