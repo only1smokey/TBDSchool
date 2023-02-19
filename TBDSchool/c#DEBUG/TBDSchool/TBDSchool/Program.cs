85f15b3ca78f9a155c1db731711f8f1c8c6be80d
watchnite23 <bozhidarbotev683@gmail.com>
2023-02-19 19:09:44 +0100

    design

--- TBDSchool/c#DEBUG/TBDSchool/TBDSchool/Program.cs
+++ TBDSchool/c#DEBUG/TBDSchool/TBDSchool/Program.cs
@@ -16,7 +16,7 @@ namespace TBDSchool
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new mainForm());
         }
     }
 }
