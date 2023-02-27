606ee446d17bd23ed4e09f03e8692d7851c15dd1
watchnite23 <bozhidarbotev683@gmail.com>
2023-02-27 19:58:20 +0100

    Tyrone Error Fix

--- TBDSchool/back/training.py
+++ TBDSchool/back/training.py
@@ -21,7 +21,7 @@ ignore_letters = ['?', '!', '.', ',']
 
 for intent in intents['intents']:
     for pattern in intent['patterns']:
-        word_list = nltk.tokenize(pattern)
+        word_list = nltk.word_tokenize(pattern)
         words.extend(word_list)
         documents.append((word_list, intent['tag']))
         if intent['tag'] not in classes:
@@ -30,4 +30,11 @@ for intent in intents['intents']:
 words = [lemmatizer.lemmatize(word) for word in words if word not in ignore_letters]
 words = sorted(set(words))
 
+classes = sorted(set(classes))
+
+pickle.dump(words, open('words.pkl', 'wb'))
+pickle.dump(words, open('classes.pkl', 'wb'))
+
 print(words)
+
+#15:35 / 35:41 . Prepare Training Data from https://www.youtube.com/watch?v=1lwddP0KUEg&t=1478s
