d322250f54804b76078d478d23cf787bbfd0dd4b
only1Ty <sturgit2008@gmail.com>
2023-02-27 19:10:01 +0100

    Python training.py updated

--- TBDSchool/back/training.py
+++ TBDSchool/back/training.py
@@ -4,3 +4,30 @@ import pickle
 import numpy as np
 import nltk
 from nltk.stem import WordNetLemmatizer
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers import Dense, Activation, Dropout
+#from tensorflow.python.keras.optimizers import SGD
+#placeholder
+#placeholder
+#placeholder
+lemmatizer = WordNetLemmatizer()
+
+intents = json.loads(open('intents.json').read())
+
+words = []
+classes = []
+documents = []
+ignore_letters = ['?', '!', '.', ',']
+
+for intent in intents['intents']:
+    for pattern in intent['patterns']:
+        word_list = nltk.tokenize(pattern)
+        words.extend(word_list)
+        documents.append((word_list, intent['tag']))
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+
+words = [lemmatizer.lemmatize(word) for word in words if word not in ignore_letters]
+words = sorted(set(words))
+
+print(words)
