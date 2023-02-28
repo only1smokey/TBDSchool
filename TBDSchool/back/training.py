765a4633a9135673b13b7875b218d955b5d42091
only1Ty <sturgit2008@gmail.com>
2023-02-28 20:41:28 +0100

    Update training.py

--- TBDSchool/back/training.py
+++ TBDSchool/back/training.py
@@ -1,3 +1,4 @@
+#https://www.youtube.com/watch?v=1lwddP0KUEg&t=1478s
 import random
 import json
 import pickle
@@ -35,6 +36,35 @@ classes = sorted(set(classes))
 pickle.dump(words, open('words.pkl', 'wb'))
 pickle.dump(words, open('classes.pkl', 'wb'))
 
-print(words)
+training = []
+output_empty = [0] * len(classes)
 
-#15:35 / 35:41 . Prepare Training Data from https://www.youtube.com/watch?v=1lwddP0KUEg&t=1478s
+for document in documents:
+    bag = []
+    word_patterns = document[0]
+    word_patterns = [lemmatizer.lemmatize(word.lower()) for word in word_patterns]
+    for word in words:
+        bag.append(1) if word in word_patterns else bag.append(0)
+
+    output_row = list(output_empty)
+    output_row[classes.index(document[1])] = 1
+    training.append([bag, output_row])
+
+random.shuffle(training)
+training = np.array(training)
+
+train_x = list(training[:, 0])
+train_y = list(training[:, 1])
+
+model = Sequential()
+model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(len(train_y[0]), activation='softmax'))
+
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True) #if the program doesn't work in future here is the problem(SGD isn't installed)
+model.compile(loss='categorical_crosssentopy', optimizer=sgd, metrics=['accurs'])
+
+model.fit(np.array(train_x), np.array(train_y), epochs=1000, batch_size=5, verbose=1)
+print("Done")
