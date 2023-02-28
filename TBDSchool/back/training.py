3cc65911abd8b4239ad77649416014b9db39ea0a
watchnite23 <bozhidarbotev683@gmail.com>
2023-02-28 22:34:59 +0100

    ChatBot test WORKS better intents
    ja

--- TBDSchool/back/training.py
+++ TBDSchool/back/training.py
@@ -1,62 +1,101 @@
-import random
-import json
+import nltk
+from nltk.stem import WordNetLemmatizer
 import numpy as np
+import random
+import pickle
+import json
 from tensorflow import keras
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Dropout
 from keras.optimizers import SGD
 
-data_file = "intents.json"
+# Uncomment the following line if you need to download the 'punkt' and 'wordnet' packages
+#nltk.download('punkt')
+#8nltk.download('wordnet')
 
-with open(data_file, 'r') as file:
+lemmatizer = WordNetLemmatizer()
+
+def tokenize(sentence):
+    return nltk.word_tokenize(sentence)
+
+def stem(word):
+    return lemmatizer.lemmatize(word.lower())
+
+def bag_of_words(tokenized_sentence, words):
+    sentence_words = [stem(word) for word in tokenized_sentence]
+    bag = np.zeros(len(words), dtype=np.float32)
+    for idx, word in enumerate(words):
+        if word in sentence_words:
+            bag[idx] = 1.0
+    return bag
+
+def create_model(input_shape, output_shape):
+    model = Sequential([
+        Dense(128, input_shape=input_shape, activation='relu'),
+        Dropout(0.5),
+        Dense(64, activation='relu'),
+        Dropout(0.5),
+        Dense(output_shape, activation='softmax')
+    ])
+    return model
+
+# Load data from the intents file
+with open('intents.json', 'r') as file:
     intents = json.load(file)
 
+# Extract words and classes from the intents file
 words = []
 classes = []
 documents = []
-ignore_letters = ['?', '!', '.', ',']
-
 for intent in intents['intents']:
     for pattern in intent['patterns']:
-        word_list = pattern.split()
-        words.extend(word_list)
-        documents.append((word_list, intent['tag']))
+        # Tokenize and stem each pattern
+        words_in_pattern = tokenize(pattern)
+        words.extend(words_in_pattern)
+        stemmed_words_in_pattern = [stem(word) for word in words_in_pattern]
+        # Add the pattern and its associated class to the list of documents
+        documents.append((stemmed_words_in_pattern, intent['tag']))
+        # Add the class to the list of classes
         if intent['tag'] not in classes:
             classes.append(intent['tag'])
 
-words = [word.lower() for word in words if word not in ignore_letters]
+# Remove duplicates from the words and classes lists, and sort them
 words = sorted(list(set(words)))
 classes = sorted(list(set(classes)))
 
+# Create the training data
 training = []
-output = []
-
 output_empty = [0] * len(classes)
-
 for document in documents:
-    bag = []
-    word_patterns = document[0]
-    word_patterns = [word.lower() for word in word_patterns]
-    for word in words:
-        bag.append(1) if word in word_patterns else bag.append(0)
+    bag = bag_of_words(document[0], words)
     output_row = list(output_empty)
     output_row[classes.index(document[1])] = 1
-    training.append(bag)
-    output.append(output_row)
+    training.append([bag, output_row])
 
+# Shuffle the training data
+random.shuffle(training)
 training = np.array(training)
-output = np.array(output)
 
-model = Sequential()
-model.add(Dense(128, input_shape=(len(training[0]),), activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(64, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(len(output[0]), activation='softmax'))
+# Split the training data into input (X) and output (y) variables
+X = list(training[:, 0])
+y = list(training[:, 1])
 
+# Define the model
+model = create_model(input_shape=(len(X[0]),), output_shape=len(classes))
+
+# Compile the model
 sgd = SGD(learning_rate=0.01, momentum=0.9, nesterov=True)
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
-model.fit(training, output, epochs=20000, batch_size=5, verbose=1)
+# Train the model
+model.fit(np.array(X), np.array(y), epochs=50000, batch_size=5, verbose=1)
+
+# Save the words and classes to pickle files
+with open('words.pkl', 'wb') as file:
+    pickle.dump(words, file)
+
+with open('classes.pkl', 'wb') as file:
+    pickle.dump(classes, file)
+
+# Save the model to an HDF5 file
 model.save('chatbot_model.h5')
-print('model created')
